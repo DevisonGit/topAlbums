abc94d0057de9701bfc6ef54f5f6b039ae6f8fa8
devisono <devison_santos@yahoo.com.br>
2024-07-29 21:35:22 -0300

    Criando CRUD - GET

--- tests/test_album.py
+++ tests/test_album.py
@@ -5,7 +5,7 @@ from .factories import AlbumFactory
 
 def test_create_album(client):
     response = client.post(
-        '/album/',
+        '/albums/',
         json={
             'id': 1,
             'name': 'test name',
@@ -31,7 +31,7 @@ def test_create_existing_album(client, session):
     session.commit()
 
     response = client.post(
-        '/album/',
+        '/albums/',
         json={
             'id': 1,
             'name': 'test name',
@@ -44,3 +44,32 @@ def test_create_existing_album(client, session):
 
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert response.json() == {'detail': 'Album already exists'}
+
+
+def test_list_albums_should_return_10_albums(client, session):
+    expected_albums = 10
+    session.bulk_save_objects(AlbumFactory.create_batch(10))
+    session.commit()
+
+    response = client.get('/albums/?name=album name')
+
+    assert len(response.json()['Albums']) == expected_albums
+
+
+def test_list_albums_should_return_1_albums(client, session):
+    expected_albums = 1
+    session.bulk_save_objects(AlbumFactory.create_batch(10))
+    session.commit()
+
+    response = client.get('/albums/?year=2015')
+
+    assert len(response.json()['Albums']) == expected_albums
+
+
+def test_list_albums_no_filter(client):
+    response = client.get('/albums/?')
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {
+        'detail': 'At least one filter parameter must be provided.'
+    }
